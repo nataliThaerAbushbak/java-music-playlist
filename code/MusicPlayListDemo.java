034211ba8df2e3dc23e52d4dfca10a2fe1c77f6f
nataliThaerAbushbak <natali2abushbak@gmail.com>
2026-05-06 22:10:42 -0700

    Rename class from MusicPlayListDimo to MusicPlayListDemo

--- code/MusicPlayListDemo.java
+++ code/MusicPlayListDemo.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package musicplaylistdimo;
+package musicplaylistdemo;
 
 /**
  *
  * @author msi
  */
-public class MusicPlayListDimo {
+public class MusicPlayListDemo {
 
     /**
      * @param args the command line arguments
