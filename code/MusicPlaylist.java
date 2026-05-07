9dd37027b2aef7827185fba492b72863c1dbba0f
nataliThaerAbushbak <natali2abushbak@gmail.com>
2026-05-06 22:11:00 -0700

    Refactor MusicPlaylist class and update package name

--- code/MusicPlaylist.java
+++ code/MusicPlaylist.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package musicplaylistdimo;
+package musicplaylistdemo;
 
 import java.util.Iterator;
 
